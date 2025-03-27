5b5ff908ed397febb063cf56416518c5f8a1f253
StanleyHaw <stanleyhaw94@gmail.com>
2025-03-27 10:41:56 +0800

    chore: initial env and install necessary plugin

---
+++ .eslintrc.ts
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      typescript: {}
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'jsx-a11y/anchor-is-valid': 'off'
+  }
+};
